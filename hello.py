04b6b763ab9a405a6366638960253d51b14b7b40
bo_uncle <328467238@qq.com>
2018-03-06 22:54:23 +0800

    hello

--- hello.py
+++ hello.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def hello():
     return '<h1><center>Hello,World!</center></h1>'
 
+@app.route('/user/<username>')
+def user(username):
+    return '<h1>Hello,myname is %s</h1>' % username
+
 if __name__ == '__main__':
     app.run(debug=True)
