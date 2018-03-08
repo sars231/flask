8de4fa35ea927890ca8f78e198884f2b53feeede
bo_uncle <328467238@qq.com>
2018-03-09 06:59:17 +0800

    solve the problem that cantimport flask

---
+++ hello.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+#coding utf-8
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1><center>Hello,World!</center></h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
