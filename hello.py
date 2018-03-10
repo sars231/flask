89d6ccee8db79337e1e2a203b0fbca9642503720
bo_uncle <328467238@qq.com>
2018-03-10 11:07:28 +0800

    flask-script

--- hello.py
+++ hello.py
@@ -1,28 +1,20 @@
-#! /usr/bin/env python
-<<<<<<< HEAD
-#coding utf-8
-=======
-#coding:utf-8
-"""
-一个简单的ｆｌａｓｋ程序
-"""
->>>>>>> 9b6facd752848bb986dd8020634bbd948c5faab9
-
 from flask import Flask
+from flask_script import Manager
 
 app = Flask(__name__)
 
+manager = Manager(app)
+
+
 @app.route('/')
-def hello():
-    return '<h1><center>Hello,World!</center></h1>'
+def index():
+    return '<h1>Hello World!</h1>'
 
-<<<<<<< HEAD
-=======
-#包含动态路由
-@app.route('/user/<username>')
-def user(username):
-    return '<h1>Hello,myname is %s</h1>' % username
 
->>>>>>> 9b6facd752848bb986dd8020634bbd948c5faab9
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
