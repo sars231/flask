92297203d318cb831c2f3db076bb82b81a4d610c
bo_uncle <328467238@qq.com>
2018-03-11 21:23:14 +0800

    session,url_for,redirect

--- hello.py
+++ hello.py
@@ -1,19 +1,32 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,session,url_for
 from flask_script import Manager
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 from datetime import datetime
 
 app = Flask(__name__)
+# 设置flask-wtf，设置秘钥
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 manager = Manager(app)
 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?',validators=[DataRequired()])
+    submit = SubmitField('submit')
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])#加入ＰＯＳＴ方法
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    # name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data   
+        return redirect(url_for('index'))#重定向
+    return render_template('index.html',current_time=datetime.utcnow(),form=form,name=session.get('name'))#从回话中读取记录的内容
 
 
 @app.route('/user/<name>')
