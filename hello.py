490d42d1e7451261768bb36de14779cbb60a644c
bo_uncle <328467238@qq.com>
2018-03-11 10:54:11 +0800

    add flask-bootstrap,flask-moment

--- hello.py
+++ hello.py
@@ -1,19 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 manager = Manager(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html',current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return '<h1>Hello,%s</h1>' % name
+
+@app.route('/num/<number>')
+def num(number):
+    return render_template('num.html',number=number)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
 
 
 if __name__ == '__main__':
